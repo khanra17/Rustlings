f870d48dbaf990362ab423e2e92f435713b2a470
Suman Khanra <suman8khanra@gmail.com>
2022-12-25 10:16:54 +0530

    📆 Day 5

--- Understanding Ownership/What is ownership/Build String from Literals/src/main.rs
+++ Understanding Ownership/What is ownership/Build String from Literals/src/main.rs
@@ -2,6 +2,8 @@ fn main() {
     let part1 = "Rust ";
     let part2 = "is the ";
     let part3 = "greatest programming language";
-    // Write your code here
+    let mut message = String::from(part1);
+    message.push_str(part2);
+    message.push_str(part3);
     println!("{}", message)
 }
