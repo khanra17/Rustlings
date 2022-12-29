0be435ebe5cbd174f52b524386175207ed4838f9
Suman Khanra <suman8khanra@gmail.com>
2022-12-29 12:55:32 +0530

    📆 Day 8

--- Structs, Methods, Enums, and Pattern Matching/Enums/Deliver Complex Messages/src/main.rs
+++ Structs, Methods, Enums, and Pattern Matching/Enums/Deliver Complex Messages/src/main.rs
@@ -1,6 +1,9 @@
 #[derive(Debug)]
 enum Message {
-    // Define the different variants used below
+    Move { x: i32, y: i32 },
+    Echo(String),
+    ChangeColor(i32, i32, i32),
+    Quit,
 }
 
 impl Message {
--- Structs, Methods, Enums, and Pattern Matching/Structs with Methods/Sending Packages/src/main.rs
+++ Structs, Methods, Enums, and Pattern Matching/Structs with Methods/Sending Packages/src/main.rs
@@ -10,23 +10,23 @@ impl Package {
         if weight_in_grams <= 0 {
             // This shouldn't happen so we panic 
             panic!();
-        } 
-        /* Instantiate the package here */
+        }
+        Package { sender_country, recipient_country, weight_in_grams }
     }
 
-    fn is_international(&self) -> /* Add return type */{
-        /* Something goes here */
+    fn is_international(&self) -> bool {
+        self.sender_country != self.recipient_country
     }
 
-    fn get_fees(&self, cents_per_gram: i32) -> /* Add return type */ {
-        /* Something goes here */
+    fn get_fees(&self, cents_per_gram: i32) -> i32 {
+        self.weight_in_grams * cents_per_gram
     }
 }
 
 fn main() {
     let sender_country = String::from("Spain");
     let recipient_country = String::from("Germany");
-    
+
     let package = Package::new(sender_country, recipient_country, 1200);
 
     if package.is_international() {
@@ -47,7 +47,7 @@ fn main() {
     } else {
         println!("{}", "Oops")
     }
-    
+
     let price = package.get_fees(cents_per_gram);
     println!("{}", "Your price is:");
     println!("{}", price);
